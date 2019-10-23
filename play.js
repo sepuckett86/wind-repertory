495b5f4d92028601bdd0e842c1248e662ae17725
sepuckett86 <sepuckett86@gmail.com>
2019-10-23 12:59:02 -0700

    add idea for better page grabbing

--- play.js
+++ play.js
@@ -1,3 +1,5 @@
-const scrapeCompositionTitles = require('./lib/services/scrapeCompositionTitles');
+// const scrapeCompositionTitles = require('./lib/services/scrapeCompositionTitles');
+const fetchCompositions = require('./lib/services/fetchCompositions');
 
-scrapeCompositionTitles();
+// scrapeCompositionTitles();
+fetchCompositions().then(comps => console.log(comps.length));
